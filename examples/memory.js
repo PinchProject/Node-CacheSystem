05c06eac9f1cc7b66787447a4a8caef0c01e345e
Ismael Gorissen <igorissen@PP-Ismael.local>
2013-04-24 15:40:41 +0200

    initial commit

---
+++ examples/memory.js
@@ -0,0 +1,26 @@
+/**
+ * Author: Ismael Gorissen
+ * Date: 24/04/13 13:24
+ * Company: PinchProject
+ */
+
+var CacheSys = require('../lib');
+
+var cache = new CacheSys();
+
+cache.set('key1', 'value1');
+console.log(cache.get('key1'));
+console.log(cache.size());
+console.log(cache.byteLength());
+
+cache.set('key2', 'value2');
+console.log(cache.get('key2'));
+console.log(cache.size());
+console.log(cache.byteLength());
+
+cache.clear();
+
+console.log(cache.get('key1'));
+console.log(cache.get('key2'));
+console.log(cache.size());
+console.log(cache.byteLength());
